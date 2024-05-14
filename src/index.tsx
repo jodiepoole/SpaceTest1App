4913ae8db375558b46dc92afad0b7881121922bc
Jodie Poole <jodie.mia.poole@gmail.com>
2024-05-14 08:16:57 +0100

    update app import in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styling/global.scss";
-import Test from "./test1.tsx";
+import Test from "./Test1.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
